3653f73aa5fc8f6c80c7416002edcaa8d6e99d1f
Tim Peters <mail@darksecond.nl>
2014-02-10 09:40:31 +0100

    Added comment and graph for Event and Sequence getValue().

--- src/Event.h
+++ src/Event.h
@@ -6,6 +6,14 @@ class Event {
 public:
 	Event(double start, float duration);
 
+	/**
+	 * Get the current value of the Event. This will go from 0 to 1 over the duration of the event.
+	 * It will be zero before the event is started and one after it is over.
+	 *            
+	 *            /----------
+	 *           /
+	 * ---------/
+	 */
 	float getValue() const;
 	bool hasPassed() const;
 	bool isOver() const;
--- src/Sequence.h
+++ src/Sequence.h
@@ -14,6 +14,23 @@ class Sequence {
 	std::vector<Trigger> _triggers;
 public:
 	void createTrigger(double start, float duration);
+
+	/**
+	 * The amount of triggers that have happend, will be 0 if none have triggered yet.
+	 */
 	int getPosition() const;
+
+	/**
+	 * Get the current value of the sequence.
+	 * If two pulses trigger around the same time their 
+	 * individual values are added together, with a maximum of 1.0.
+	 * In graph form the value would look something like this, over time.
+	 * 
+	 *        \           \
+	 *       | \         | \
+	 *       |  \        |  \
+	 *       |   \       |   \
+	 * ------|    \------|    \------
+	 */
 	float getValue() const;
 };
